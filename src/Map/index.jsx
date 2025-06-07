1e98fba087d2d3e6cd90a4538643c6be361fd201
Serg <benderlio@gmail.com>
2025-06-07 17:27:01 +0200

    Refactor code structure for improved readability and maintainability

--- src/Map/index.jsx
+++ src/Map/index.jsx
@@ -34,21 +34,21 @@ const Map = ({ data, onLoad, onFeatureClick }) => {
       setMapLoaded(true);
 
       // set custom background color
-      // map.getStyle().layers.forEach((layer) => {
-      //   if (layer.type === "background") {
-      //     map.setPaintProperty(layer.id, "background-color", "#e1eff0");
-      //   }
-      //   // all land fill layers: land, landuse*, landcover*
-      //   if (
-      //     layer.type === "fill" &&
-      //     (layer.id === "land" ||
-      //       layer.id.startsWith("landuse") ||
-      //       layer.id.startsWith("landcover") ||
-      //       layer.id.toLowerCase().includes("park"))
-      //   ) {
-      //     map.setPaintProperty(layer.id, "fill-color", "#e1eff0");
-      //   }
-      // });
+      map.getStyle().layers.forEach((layer) => {
+        if (layer.type === "background") {
+          map.setPaintProperty(layer.id, "background-color", "#e1eff0");
+        }
+        // all land fill layers: land, landuse*, landcover*
+        if (
+          layer.type === "fill" &&
+          (layer.id === "land" ||
+            layer.id.startsWith("landuse") ||
+            layer.id.startsWith("landcover") ||
+            layer.id.toLowerCase().includes("park"))
+        ) {
+          map.setPaintProperty(layer.id, "fill-color", "#e1eff0");
+        }
+      });
     });
   }, []);
 
